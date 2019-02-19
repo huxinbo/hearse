3f678a002d8ee36c6e93934c0b4b66161a5d0abc
huxinbo <46860739+huxinbo@users.noreply.github.com>
2019-02-19 22:24:07 +0800

    add hearse

---
+++ hearse/src/main/java/com/hearsegroup/service/hearse/HearseApplication.java
@@ -0,0 +1,25 @@
+package com.hearsegroup.service.hearse;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import redis.clients.jedis.Jedis;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableConfigurationProperties
+@MapperScan("com.cheezgroup.service.purchase.mapper")
+public class HearseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HearseApplication.class, args);
+
+        Jedis jedis = new Jedis("6379");
+        System.out.println("连接成功");
+        // 查看服务是否运行
+        System.out.println("服务正在运行: " + jedis.ping());
+    }
+
+}
---
+++ hearse/src/test/java/com/hearsegroup/service/hearse/HearseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hearsegroup.service.hearse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HearseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

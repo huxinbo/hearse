d0ff3091c674f873a8cce244e513d16769f41387
huxinbo <1073118130@qq.com>
2019-03-10 20:44:15 +0800

    增加IniRealm认证和授权

--- hearse/src/main/java/com/hearsegroup/service/hearse/shiroTest.java
+++ hearse/src/main/java/com/hearsegroup/service/hearse/shiroTest.java
@@ -1,8 +1,11 @@
 package com.hearsegroup.service.hearse;
+import org.apache.catalina.security.SecurityUtil;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.realm.text.IniRealm;
 import org.apache.shiro.subject.Subject;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,7 +25,7 @@ public class shiroTest {
     @Test
     public void shiroTest() {
 
-        //1.构建SecurityManager环境
+     /*   //1.构建SecurityManager环境
         DefaultSecurityManager securityManager = new DefaultSecurityManager();
         securityManager.setRealm(simpleAccountRealm);
 
@@ -44,7 +47,28 @@ public class shiroTest {
             System.out.println("成功");
         } else {
             System.out.println("失败");
+        }*/
+
+
+     //IniReaml认证和授权
+        DefaultSecurityManager securityManager = new DefaultSecurityManager();
+        IniRealm iniRealm = new IniRealm("classpath:user.ini");
+        securityManager.setRealm(iniRealm);
+
+        SecurityUtils.setSecurityManager(securityManager);
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken("huxinbo", "123456");
+        subject.login(token);
+
+        if (subject.isAuthenticated()) {
+            System.out.println("成功");
+        } else {
+            System.out.println("失败");
         }
+
+        subject.checkRole("admin");
+        subject.checkPermissions("user:insert");
 
 
     }
